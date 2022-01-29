1a8e62a0ef4eb642b22534d809cbc153cc8295b5
Samantha Diaz <samidiaz3@gmail.com>
2022-01-29 16:47:00 -0500

    commit tree ad6a36657a90e107350b76c2706765af0d76156d

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Add your functions here
+// generalized map
+const map = function(array, callback){
+    let newArray = []
+    for( let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i]))
+    }
+    return newArray
+}
+// Solution:
+    // function map(src, callback) {
+    //     const r = [];
+    
+    //     for (let i = 0; i < src.length; i++) {
+    //       const theElement = src[i];
+    //       r.push(callback(theElement));
+    //     }
+    
+    //     return r;
+    //  }
+
+
+// generalized reduce
+function reduce(array, callback, startingValue) {
+    
+    if (startingValue) {
+        let total = startingValue
+        for (let i = 0; i < array.length; i++) {
+            total = callback(array[i], total)
+        }
+        return total 
+    } 
+    else {
+        let value = array[0] //to set it as a value, not array
+        for (let i = 1; i < array.length; i++) {
+            value = callback(array[i], value)
+        }
+        return value
+    }
+}
+// Solution: 
+    // function reduce(array, callback, startingValue) {
+    //     let total;
+    //     if (startingValue) {
+    //       total = startingValue;
+    //       for (let i = 0; i < array.length; i++) {
+    //         total = callback(array[i], total);
+    //       }
+    //       return total;
+    //     } else {
+    //       total = array[0];
+    //       for (let i = 1; i < array.length; i++) {
+    //         total = callback(array[i], total);
+    //       }
+    //       return total;
+    //     }
+    //   }
+
+
+
